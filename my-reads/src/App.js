a2dfdcde5ce94919adbc727003b6a18a1e85ba64
code4jhon <code4jhon@gmail.com>
2018-10-09 21:08:38 -0500

    Define Book props, pass book data as props.

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Book from './Book';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const coverUrl = 'http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api';
     return (
       <div className="App">
-        <Book></Book>
+        <Book title="stuff" authors="J.R.R. Tolkien" coverUrl={coverUrl}></Book>
       </div>
     );
   }
--- my-reads/src/Book.js
+++ my-reads/src/Book.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+  static propTypes = {
+    coverUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.string.isRequired
+  }
+
   render() {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${this.props.coverUrl})` }}></div>
           <div className="book-shelf-changer">
             <select>
               <option value="move" disabled>Move to...</option>
@@ -16,8 +23,8 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">The Hobbit</div>
-        <div className="book-authors">J.R.R. Tolkien</div>
+        <div className="book-title">{this.props.title}</div>
+        <div className="book-authors">{this.props.authors}</div>
       </div>
     )
   }
